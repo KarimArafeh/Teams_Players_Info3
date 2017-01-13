4d9e375b9907031323cd0ebd07380640726721b4
karim <y2793623b@iespoblenou.org>
2017-01-13 16:13:55 +0100

    definir el getview para mostrar les dades que volem

--- app/src/main/java/com/example/y2793623b/teams_players_info/CompetitionAdapter.java
+++ app/src/main/java/com/example/y2793623b/teams_players_info/CompetitionAdapter.java
@@ -1,8 +1,12 @@
 package com.example.y2793623b.teams_players_info;
 
 import android.content.Context;
-import android.graphics.Movie;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -16,5 +20,29 @@ public class CompetitionAdapter extends ArrayAdapter<Competition>{
         super(context, resource, objects);
     }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Obtenim l'objecte en la possició corresponent
+        Competition compet = getItem(position);
+        Log.w("XXXX-------------", compet.toString());
+
+        // Mirem a veure si la View s'està reusant, si no es així "inflem" la View
+
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.lv_list_row, parent, false);
+        }
+
+        // Unim el codi en les Views del Layout
+        TextView caption = (TextView) convertView.findViewById(R.id.Titulo);
+        TextView  league = (TextView) convertView.findViewById(R.id.txt_league);
+
+        // Fiquem les dades dels objectes (provinents del JSON) en el layout
+        caption.setText(compet.getCaption());
+
+        // Retornem la View replena per a mostrarla
+        return convertView;
+    }
 
 }
